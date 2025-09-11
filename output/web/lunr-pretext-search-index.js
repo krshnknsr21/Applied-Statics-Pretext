5cd7c24cb25b16aaa7b1fa5c914d2c698af62fb7
Krishna <krshnknsr21@gmail.com>
2025-09-11 12:55:21 -0700

    Initial commit with first chapter

---
+++ output/web/lunr-pretext-search-index.js
@@ -0,0 +1,185 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "AREAS",
+  "level": "1",
+  "url": "AREAS.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "AREAS",
+  "body": " AREAS  The area of a rectangle is found by: (base) x (height)  The area of a triangle is found by: ½ (base) x (height)  The area of a circle is found by: .  "
+},
+{
+  "id": "FUNDAMENTALS-OF-RIGHT-TRIANGLES",
+  "level": "1",
+  "url": "FUNDAMENTALS-OF-RIGHT-TRIANGLES.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "FUNDAMENTALS OF RIGHT TRIANGLES",
+  "body": " FUNDAMENTALS OF RIGHT TRIANGLES   The sum of all of the angles in any triangle must add up to 180°.    Right-Angle Triangles  A right angle triangle means there is one angle that is exactly 90°. The side opposite to the right angle is called the Hypotenuse (C). The hypotenuse is the longest side in the right-angled triangle.  For all right angle triangles, you can use the Pythagorean Theorem ( ) to solve for unknown lengths.    PYTHAGOREAN THEOREM ( )    A right triangle with sides A, B, and hypotenuse C.     The names of the variables are irrelevant; you can use any letter to denote any of these. Another way to consider this theorem is:   ( )    "
+},
+{
+  "id": "right-angle-triangles-2",
+  "level": "2",
+  "url": "FUNDAMENTALS-OF-RIGHT-TRIANGLES.html#right-angle-triangles-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "right angle Hypotenuse "
+},
+{
+  "id": "right-angle-triangles-3",
+  "level": "2",
+  "url": "FUNDAMENTALS-OF-RIGHT-TRIANGLES.html#right-angle-triangles-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "right angle Pythagorean Theorem "
+},
+{
+  "id": "right-angle-triangles-4-1",
+  "level": "2",
+  "url": "FUNDAMENTALS-OF-RIGHT-TRIANGLES.html#right-angle-triangles-4-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "PYTHAGOREAN THEOREM "
+},
+{
+  "id": "fig-right-triangle",
+  "level": "2",
+  "url": "FUNDAMENTALS-OF-RIGHT-TRIANGLES.html#fig-right-triangle",
+  "type": "Figure",
+  "number": "1.2.1",
+  "title": "",
+  "body": " A right triangle with sides A, B, and hypotenuse C.    "
+},
+{
+  "id": "TRIGONOMETRY",
+  "level": "1",
+  "url": "TRIGONOMETRY.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "TRIGONOMETRY",
+  "body": " TRIGONOMETRY   Right-angled triangles have specific trigonometric properties, which can be used to find unknown dimensions or angles. A side can be opposite to one angle and adjacent to a different angle. The location of the angle determines the relationships “opposite” or “adjacent”.     Basic Definitions (SOH-CAH-TOA)   The three primary trigonometric ratios are:     (SOH)     (CAH)     (TOA)       Scenario #1 — Angle as drawn below     Scenario #2 — Angle as drawn below    "
+},
+{
+  "id": "TRIGONOMETRY-2-1",
+  "level": "2",
+  "url": "TRIGONOMETRY.html#TRIGONOMETRY-2-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "The location of the angle determines the relationships “opposite” or “adjacent”. "
+},
+{
+  "id": "definitions-soh-cah-toa-2",
+  "level": "2",
+  "url": "TRIGONOMETRY.html#definitions-soh-cah-toa-2",
+  "type": "List",
+  "number": "1.3.1",
+  "title": "The three primary trigonometric ratios are:",
+  "body": " The three primary trigonometric ratios are:     (SOH)     (CAH)     (TOA)    "
+},
+{
+  "id": "PYTHAGOREAN-THEOREM",
+  "level": "1",
+  "url": "PYTHAGOREAN-THEOREM.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "PYTHAGOREAN THEOREM",
+  "body": " PYTHAGOREAN THEOREM  For all right angle triangles, you can use the Pythagorean Theorem (C2 = A2 + B2) or [SOH | CAH | TOA] to solve for unknown angles or lengths.    PYTHAGOREAN THEOREM ( )   (SOH)   (CAH)   (TOA)    For example, to solve for angle , you can use the following equations: , , or .    "
+},
+{
+  "id": "PYTHAGOREAN-THEOREM-3-1",
+  "level": "2",
+  "url": "PYTHAGOREAN-THEOREM.html#PYTHAGOREAN-THEOREM-3-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "PYTHAGOREAN THEOREM "
+},
+{
+  "id": "pythagorean_theorem_1",
+  "level": "2",
+  "url": "PYTHAGOREAN-THEOREM.html#pythagorean_theorem_1",
+  "type": "Figure",
+  "number": "1.4.1",
+  "title": "",
+  "body": " For example, to solve for angle , you can use the following equations: , , or .   "
+},
+{
+  "id": "SIMILAR-TRIANGLES",
+  "level": "1",
+  "url": "SIMILAR-TRIANGLES.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "SIMILAR TRIANGLES",
+  "body": " SIMILAR TRIANGLES  You can use “similar triangles” to compute angles, lengths, or forces of two triangles that are considered similar.  For example, to solve for X in the diagram, we can use similar triangles (if the angles in the triangle are the same).      So 24'    "
+},
+{
+  "id": "similar_triangles_example_1",
+  "level": "2",
+  "url": "SIMILAR-TRIANGLES.html#similar_triangles_example_1",
+  "type": "Example",
+  "number": "1.5.1",
+  "title": "",
+  "body": "    So 24'  "
+},
+{
+  "id": "EXAMPLES",
+  "level": "1",
+  "url": "EXAMPLES.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "EXAMPLES",
+  "body": " EXAMPLES     Solve for all unknowns (X and )   (for accessibility)      Step 1: Is triangle “right-angled” or “oblique-angled” ?     Right-angled triangle     Step 2: Solve for X    Use Pythagorean Theorem             Step 3: Solve for                    Solve for all unknowns ( , X and Z)   (for accessibility)      Step 1: Is triangle “right-angled” or “oblique-angled” ?     Right-angled triangle     Step 2: Solve for    Properties of a triangle are that total internal angles =              Step 3: Solve for X            Step 4: Solve for Z              "
+},
+{
+  "id": "EXAMPLES-2",
+  "level": "2",
+  "url": "EXAMPLES.html#EXAMPLES-2",
+  "type": "Example",
+  "number": "1.6.1",
+  "title": "",
+  "body": "   Solve for all unknowns (X and )   (for accessibility)      Step 1: Is triangle “right-angled” or “oblique-angled” ?     Right-angled triangle     Step 2: Solve for X    Use Pythagorean Theorem             Step 3: Solve for                "
+},
+{
+  "id": "EXAMPLES-3",
+  "level": "2",
+  "url": "EXAMPLES.html#EXAMPLES-3",
+  "type": "Example",
+  "number": "1.6.2",
+  "title": "",
+  "body": "   Solve for all unknowns ( , X and Z)   (for accessibility)      Step 1: Is triangle “right-angled” or “oblique-angled” ?     Right-angled triangle     Step 2: Solve for    Properties of a triangle are that total internal angles =              Step 3: Solve for X            Step 4: Solve for Z             "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
